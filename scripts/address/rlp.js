c4c39ad8c6ad8b19133741a43333e0fe63f303f9
谢嘉铮 <xiejiazheng@xiejiazhengdeMacBook-Pro.local>
2023-02-20 17:24:56 +0800

    rlp

---
+++ scripts/address/rlp.js
@@ -0,0 +1,13 @@
+const rlp = require('rlp');
+const keccak = require('keccak');
+const web3 = require('web3')
+
+const encodedData = rlp.encode([
+  '0x06902C817113Db7f0EcCf5D17bD5d1bdf22731Ec', // address from which contract is to be deployed
+  web3.utils.toHex(2659) // hex encoded nonce of address that will be used for contract deployment
+]);
+
+const buffer = new Buffer.from(encodedData)
+
+const contractAddress = `0x${keccak('keccak256').update(buffer).digest('hex').substring(24)}`
+console.log({contractAddress})
